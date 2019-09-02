3baea6a496ff95cfd7ae70d83f5f81bff3fdd043
yangchen <yangchendoit@163.com>
2019-09-02 18:23:18 +0800

    add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,32 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   globals: {
     $: true
   },
+
   extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-unused-vars": "warn"
   },
+
   parserOptions: {
     parser: "babel-eslint"
-  }
+  },
+
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)"],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
